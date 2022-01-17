3d097a3c73f913ad12da69c7a78ea6603f1b1437
RocknR0011 <95881702+RocknR0011@users.noreply.github.com>
2022-01-17 14:12:29 +0300

    сохранили 2 коммит

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
-﻿
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Int numberA = new Random().Next(1,10);
+Console.WriteLine(numberA);
+Int numberB = new Random().Next(1,10);
+Console.WriteLine(numberB);
+Int result = numberA + numberB;
+Console.WriteLine(result);
